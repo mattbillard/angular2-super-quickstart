73662c7833c4101cd17458d6c911e02938ee1be8
mattbillard <mattbillarddesign@gmail.com>
2017-01-25 21:56:54 -0500

    Improve HTTP examples

--- app/http/http.component.ts
+++ app/http/http.component.ts
@@ -12,16 +12,19 @@ export class HttpComponent implements OnInit {
 
     constructor(
         private vehicleService: VehicleService
-    ) {
-    }
+    ) {}
 
     ngOnInit() {
-        this.getVehicles1();                                             //#1 HTTP
-        this.getVehicles2();                                             //#2 HTTP with Async Pipe
-        this.getVehicles3();                                             //#3 HTTP with promises
+        this.getVehicles1();
+        this.getVehicles2();
+        this.getVehicles3();
+        this.getVehicles4();
+        this.getVehicles5();
+        this.getVehicles6();
     }
 
-    //#1 HTTP
+
+    //#1 HTTP + observable
     vehicles1: Vehicle[];
     errorMessage1: string;
     getVehicles1() {
@@ -32,15 +35,51 @@ export class HttpComponent implements OnInit {
             );
     }
 
-    //#2 HTTP with Async Pipe
+
+    //#2 HTTP + observable + async pipe
     vehicles2: Observable<Vehicle[]>;
-    getVehicles2(value?: string) {
-        this.vehicles2 = this.vehicleService.getVehicles2(value);
+    getVehicles2() {
+        this.vehicles2 = this.vehicleService.getVehicles2();
     }
 
-    //#3 HTTP with promises
-    vehicles3: Promise<Vehicle[]>;
-    getVehicles3(value?: string) {
-        this.vehicles3 = this.vehicleService.getVehicles3(value);
+
+    //#3 HTTP + promise
+    vehicles3: Vehicle[];
+    errorMessage3: string;
+    getVehicles3() {
+        this.vehicleService.getVehicles3().then(
+            vehicles => this.vehicles3 = vehicles,
+            error =>  this.errorMessage3 = <any>error
+        );
     }
+
+
+    //#4 HTTP + promise + async pipe
+    vehicles4: Promise<Vehicle[]>;
+    getVehicles4() {
+        this.vehicles4 = this.vehicleService.getVehicles4();
+    }
+
+
+    //#5 Immediately resolved promise
+    vehicles5: any;
+    errorMessage5: string;
+    getVehicles5() {
+        this.vehicleService.getVehicles5().then(
+            vehicles => this.vehicles5 = vehicles,
+            error =>  this.errorMessage5 = <any>error
+        );
+    }
+
+
+    //#6 SetTimeout promise
+    vehicles6: any;
+    errorMessage6: string;
+    getVehicles6() {
+        this.vehicleService.getVehicles6().then(
+            vehicles => this.vehicles6 = vehicles,
+            error =>  this.errorMessage6 = <any>error
+        );
+    }
+
 }
--- app/http/services/vehicle.service.ts
+++ app/http/services/vehicle.service.ts
@@ -6,50 +6,75 @@ export class Vehicle {
     constructor(public id: number, public name: string) { }
 }
 
+
+let mockResponse = [
+    { "id": 30, "name": "Millennium Falcon", "type": "space" },
+    { "id": 32, "name": "X-Wing Fighter", "type": "space" },
+    { "id": 33, "name": "Imperial Star Destroyer", "type": "space" },
+    { "id": 34, "name": "AT-AT Walker", "type": "land" }
+];
+
 @Injectable()
 export class VehicleService {
-    constructor(private http: Http) { }
+    constructor(
+        private http:Http
+    ) {}
 
-    //#1 HTTP
+
+    //#1 HTTP + observable
     getVehicles1() {
         return this.http
             .get('api/vehicles.json')
-            .map((response: Response) => <Vehicle[]>response.json().data)
-            .do(data => console.log(data))
-            .catch(this.handleError1);
-    }
-    private handleError1(error: Response) {
-        console.error(error);
-        let msg = `Error status code ${error.status} at ${error.url}`;
-        return Observable.throw(msg);
+            .map((response:Response) => <Vehicle[]>response.json().data)
+            //.do(data => console.log(data))
+            .catch(this.handleError);
     }
 
-    //#2 HTTP with Async Pipe
-    getVehicles2(value?: string) {
+
+    //#2 HTTP + observable + async pipe
+    getVehicles2() {
         return this.http
             .get('api/vehicles.json')
-            .map((response: Response) => {
-                let vehicles = <Vehicle[]>response.json().data;
-                if (!value) { return vehicles; }
-                return vehicles.filter(v => v.name.toLowerCase().includes(value.toLowerCase()));
-            })
-            .do(data => console.log(data))
-            .catch(this.handleError2);
-    }
-    private handleError2(error: Response) {
-        console.error(error);
-        let msg = `Error status code ${error.status} at ${error.url}`;
-        return Observable.throw(msg);
+            .map((response:Response) => <Vehicle[]>response.json().data)
+            //.do(data => console.log(data))
+            .catch(this.handleError);
     }
 
-    //#3 HTTP with promises
-    getVehicles3(value?: string) {
+
+    //#3 HTTP + promise
+    getVehicles3() {
         return this.http.get('api/vehicles.json')
-            .map((response: Response) => <Vehicle[]>response.json().data)
             .toPromise()
-            .catch(this.handleError3);
+            .then(response => response.json().data)
+            .catch(this.handleError);
     }
-    private handleError3(error: Response) {
+
+
+    //#4 HTTP + promise + async pipe
+    getVehicles4() {
+        return this.http.get('api/vehicles.json')
+            .map((response:Response) => <Vehicle[]>response.json().data)
+            .toPromise()
+            .catch(this.handleError);
+    }
+
+
+    //#5 Immediately resolved promise
+    getVehicles5() {
+        return Promise.resolve(mockResponse);
+    }
+
+
+    //#6 SetTimeout promise
+    getVehicles6() {
+        return new Promise(resolve => {
+            setTimeout(() => resolve(mockResponse), 3000);
+        });
+    }
+
+
+
+    private handleError(error: Response) {
         console.error(error);
         let msg = `Error status code ${error.status} at ${error.url}`;
         return Observable.throw(msg);
